62ec77132502e8a2fb4f0869b82ae880c41b32b8
pangkopsr <124670707+pangkopsr@users.noreply.github.com>
2023-04-25 16:13:03 +0700

    Add files via upload

---
+++ tourpointphrae.js
@@ -0,0 +1,158 @@
+
+var tourpointphrae = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานแห่งชาติผาขาม",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.14297746083298,
+            18.17295665053426
+          ],
+          "type": "Point"
+        },
+        "id": 0
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานแห่งชาติดอยผากลอง",
+          "type": "ธรรมชาติ",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.06394766061487,
+            18.140600738084444
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระธาตุช่อแฮ",
+          "type": "ศาสนา",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.20444222883418,
+            18.086593235986598
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานแห่งชาติแม่ยม",
+          "type": "ธรรมชาติ",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.21481427225865,
+            18.729036946762434
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานแห่งชาติเวียงโกศัย",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            99.48106138533831,
+            17.898063456981504
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดผาสุก",
+          "type": "ศาสนา",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.11415024889936,
+            18.11594319397362
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ร้านสินค้าโอท็อป",
+          "type": "ร้านค้า",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.06682934888346,
+            17.96582841135438
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บ้านปิ่น",
+          "type": "สถานีรถไฟ",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            99.866839060335,
+            18.096295738973566
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระบาท",
+          "type": "ศาสนา",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.14029166750589,
+            18.143997788921823
+          ],
+          "type": "Point"
+        },
+        "id": 8
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "คุ้มเจ้าหลวงเมืองแพร่",
+          "type": "ประวัติศาสตร์",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.13873711103906,
+            18.144237602915908
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
